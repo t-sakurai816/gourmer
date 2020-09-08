a8b8870b801879cfb1b6d1aae6237d997708599e
t-sakurai816 <poppun_0816@icloud.com>
2020-09-09 00:10:32 +0900

    ボタンが変わる

--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,10 @@ import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import { Link, useHistory } from 'react-router-dom';
 
-const Header = ({backButton}) => {
+const Header = ({ backButton }) => {
   const history = useHistory();
   return (
     <div className="header">
@@ -15,20 +16,33 @@ const Header = ({backButton}) => {
           <ArrowBackIosIcon className="header__icon" fontSize="large" />
         </IconButton>
       ) : (
-        <IconButton>
-          <PersonIcon className="header__icon" fontSize="large" />
-        </IconButton>
-      )}
+          <Link to="copy-right">
+            <IconButton>
+              <PersonIcon className="header__icon" fontSize="large" />
+            </IconButton>
+          </Link>
+        )}
 
       <Link to="/">
         <img className="header__logo" src="https://icooon-mono.com/i/icon_11165/icon_111651_64.png" alt="Gourmer-logo" />
       </Link>
-      
-      <Link to="/chat">
+
+      {backButton ? (
+        <IconButton onClick={() => history.replace(backButton)}>
+          <ArrowForwardIosIcon className="header__icon" fontSize="large" />
+        </IconButton>
+      ) : (
+          <Link to="/chat">
+            <IconButton>
+              <ForumIcon className="header__icon" fontSize="large" />
+            </IconButton>
+          </Link>
+        )}
+      {/* <Link to="/chat">
         <IconButton>
           <ForumIcon className="header__icon" fontSize="large" />
         </IconButton>
-      </Link>
+      </Link> */}
     </div>
   )
 }
