70d61a6a6ec6c8222d51b6b7f26d03044edd26e7
t-sakurai816 <poppun_0816@icloud.com>
2020-09-09 00:09:25 +0900

    コピーライトを追加

---
+++ src/CopyRight.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './CopyRight.css'
+
+const CopyRight = () => {
+  return (
+    <div>
+      <div className="copyRight">
+        <p>ホットペッパーグルメのAPIからデータを取得しています。</p>
+        <p>そのうち現在地をもとに探せるようにします</p>
+        <p>スマホだとお店のURLがクリックできなくなっています。そのうち対応します</p>
+        <div className="hotpaper">
+          <a href="http://webservice.recruit.co.jp/"><img src="http://webservice.recruit.co.jp/banner/hotpepper-m.gif" alt="ホットペッパー Webサービス" width="88" height="35" border="0" title="ホットペッパー Webサービス" /></a>
+        </div>
+        <div class="github_logo">
+          <a href="https://github.com/t-sakurai816/query" target="_blank"><img
+            alt="github-logo" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png"
+            height="40px" />
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CopyRight;
