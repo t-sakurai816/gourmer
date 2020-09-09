f7512b7543743fa6406a39822443ff1cae58394e
t-sakurai816 <poppun_0816@icloud.com>
2020-09-09 15:41:30 +0900

    リポジトリを変更

--- src/CopyRight.js
+++ src/CopyRight.js
@@ -8,11 +8,12 @@ const CopyRight = () => {
         <p>ホットペッパーグルメのAPIからデータを取得しています。</p>
         <p>そのうち現在地をもとに探せるようにします</p>
         <p>スマホだとお店のURLがクリックできなくなっています。そのうち対応します</p>
+        <p className="query"><a href="https://query.massyu.net" target="_blank" rel="noopener noreferrer">お問い合わせはこちら</a></p>
         <div className="hotpaper">
           <a href="http://webservice.recruit.co.jp/"><img src="http://webservice.recruit.co.jp/banner/hotpepper-m.gif" alt="ホットペッパー Webサービス" width="88" height="35" border="0" title="ホットペッパー Webサービス" target="_blank" rel="noopener noreferrer"/></a>
         </div>
         <div className="github_logo">
-          <a href="https://github.com/t-sakurai816/query" target="_blank" rel="noopener noreferrer"><img
+          <a href="https://github.com/t-sakurai816/gourmer" target="_blank" rel="noopener noreferrer"><img
             alt="github-logo" src="https://github.githubassets.com/images/modules/logos_page/GitHub-Logo.png"
             height="40px" />
           </a>
